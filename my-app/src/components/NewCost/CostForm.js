c76e89868a9cf83367bec7f1f9a0ac4de1841a49
Alexander Medved <medved1991alexander@gmail.com>
2024-10-09 12:37:48 +0300

    add some states

--- my-app/src/components/NewCost/CostForm.js
+++ my-app/src/components/NewCost/CostForm.js
@@ -1,19 +1,36 @@
+import React, { useState } from 'react';
 import './CostForm.css';
 
 function CostForm(){
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    };
+
     return <form>
         <div className='new-cost__controls'>
             <div className='new-cost__control label'>
                 <label>Название</label>  
-                <input type='text'/>          
+                <input type='text' onChange={nameChangeHandler}/>          
             </div>
             <div className='new-cost__control label'>
                 <label>Сумма</label>  
-                <input type='number' min='0.01' step='0.01'/>         
+                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>         
             </div>
             <div className='new-cost__control label'>
                 <label>Дата</label>  
-                <input type='date' min='2024-10-08'/>         
+                <input type='date' min='2024-10-08' onChange={dateChangeHandler}/>         
             </div>
             <div className='new-cost__actions'>
                 <button type='submit'>Добавить Расход</button>       
