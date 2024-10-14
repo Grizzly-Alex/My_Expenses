b7d7af438ae49af9e5a7120ef6c41c4355191c2c
Alexander Medved <medved1991alexander@gmail.com>
2024-10-14 10:12:17 +0300

    hide input form

--- my-app/src/components/NewCost/CostForm.js
+++ my-app/src/components/NewCost/CostForm.js
@@ -93,7 +93,8 @@ function CostForm(props){
                 <input type='date' min='2024-10-08' value={inputDate} onChange={dateChangeHandler}/>         
             </div>
             <div className='new-cost__actions'>
-                <button type='submit'>Добавить Расход</button>       
+                <button type='submit'>Добавить Расход</button>  
+                <button type='button' onClick={props.onCancelForm}>Отмена</button>      
             </div>
         </div>
     </form>
--- my-app/src/components/NewCost/NewCost.js
+++ my-app/src/components/NewCost/NewCost.js
@@ -1,7 +1,10 @@
 import './NewCost.css';
 import CostForm from './CostForm.js';
+import React, {useState} from 'react'
 
 function NewCost(props){
+    const [isFormVisible, setFormVisible] = useState(false);
+
     const saveCostDataHandler = (inputCostData) =>
     {
         const costData = {
@@ -10,9 +13,19 @@ function NewCost(props){
         };
 
         props.onAddCost(costData);
-    }
+        setFormVisible(false);
+    };
+
+    const inputCostDataHandler = () => 
+        setFormVisible(true);
+
+    const cancelCostHandler = () => 
+        setFormVisible(false);
+    
+
     return <div className='new-cost'>
-        <CostForm onSaveCostData = {saveCostDataHandler}/>
+        {!isFormVisible && <button onClick={inputCostDataHandler}>Добавить новый расход</button>}
+        {isFormVisible && <CostForm onSaveCostData = {saveCostDataHandler} onCancelForm = {cancelCostHandler}/>}
     </div>
 
 }
